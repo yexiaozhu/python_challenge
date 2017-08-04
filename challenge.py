42e823e5b2d38f14851f2a90cb008d4a3c830b1b
557703988@139.com <557703988@139.com>
2017-08-03 23:48:28 -0700

    python challenge 01

--- challenge.py
+++ challenge.py
@@ -2,8 +2,23 @@
 #coding=utf-8
 #author=yexiaozhu
 
-start_url = 'http://www.pythonchallenge.com/pc/def/'
-num = 2**38
-# print num
-end_url = start_url + str(num) + '.html'
+str = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw " \
+      "fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. " \
+      "lmu ynnjw ml rfc spj."
+
+def str_2(s):
+    ans = ''
+    for c in s:
+        if c.isalpha():
+            tmp = ord(c) + 2
+            if tmp > ord('z'):
+                tmp -= 26
+            ans += chr(tmp)
+        else:
+            ans += c
+    return ans
+print str_2(str)
+print str_2('map')
+start_url = 'http://www.pythonchallenge.com/pc/def/map.html'
+end_url = start_url.replace('map', str_2('map'))
 print end_url
