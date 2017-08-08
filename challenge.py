40abba07f85b086e56c87ad99a0049c30245875d
557703988@139.com <557703988@139.com>
2017-08-08 00:15:38 -0700

    python challenge 05

--- challenge.py
+++ challenge.py
@@ -1,35 +1,12 @@
 #!/usr/bin/env python 2.7.12
 #coding=utf-8
 #author=yexiaozhu
-import urllib
-from urllib import *
-import re
-import os
 
+start_url = "http://www.pythonchallenge.com/pc/def/peak.html"
 
-def read(nothing):
-    try:
-        head = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='
-        s = urllib.urlopen(head + nothing).read()
-        print s
-        # 使用正则表达式匹配抓取到的页面中的最后一个数字
-        pat = re.compile('([0-9]+)')
-        nothing = re.findall(pat, s)[-1]
-        # 返回函数本身 使函数一直执行下去
-        return read(nothing)
-    except:
-        if 'Divide' in s:
-            return read(str(int(nothing) / 2))
-        else:
-            pass
+import pickle
 
-
-if __name__ == '__main__':
-    read('12345')
-    # start_url = 'http://www.pythonchallenge.com/pc/def/'
-    # end_url = start_url + str(s)
-    # print end_url
-    raw_input('<PRESS ENTER>')
-
-# end_url = start_url.replace('ocr', passcodes)
-# print end_url
+with open('banner.p', 'rb') as fp:
+    t = pickle.load(fp)
+    for item in t:
+        print(''.join([x[0] * x[1] for x in item]))
