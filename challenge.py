40e7ffc05f6471f5200ced438d4d9b3a523a32a9
557703988@139.com <557703988@139.com>
2017-08-08 01:47:35 -0700

    python challenge 07

--- challenge.py
+++ challenge.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python 2.7.12
 #coding=utf-8
 #author=yexiaozhu
+import re
 
-start_url = "http://www.pythonchallenge.com/pc/def/channel.html"
+start_url = "http://www.pythonchallenge.com/pc/def/oxygen.html"
 
-import zipfile as zf
-zfp = zf.ZipFile('channel.zip')
-nothing = '90052'
-li = []
-while True:
-    try:
-        fp = zfp.open(nothing+'.txt', 'r')
-        text = fp.read().decode()
-        nothing = text.split()[-1]
-        li.append(zfp.getinfo(nothing+'.txt').comment.decode())
-        fp.close()
-    except KeyError:
-        print 'It"s time to check manually'
-        break
-print ''.join(li)
+from PIL import Image
+img = Image.open('oxygen.png')
+# data = [img.getpixel((i, 43)) for i in range(0, 609)]
+data = [chr(img.getpixel((i, 50))[0]) for i in range(0, 609, 7)]
+# print data
+# print ''.join(data)
+result = ''.join(data)
+print result
+print type(result)
+data = re.findall(r'\d+', result)
+print data
+# print type(data)
+list = []
+for i in data:
+    # print int(i)
+    list.append(int(i))
+print list
+passcodes = ''.join([chr(j) for j in list])
+end_url =  start_url.replace('oxygen', passcodes)
+print end_url
