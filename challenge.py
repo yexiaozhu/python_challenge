99d72fe670f0bfd1ccbd96d1e8542497ef4c4cf0
557703988@139.com <557703988@139.com>
2017-08-10 01:40:41 -0700

    python challenge 12

--- challenge.py
+++ challenge.py
@@ -2,25 +2,17 @@
 #coding=utf-8
 #author=yexiaozhu
 
-from PIL import Image
+import Image
+from cStringIO import StringIO
 
-im = Image.open("cave.jpg")
+s= open("evil2.gfx", "rb").read()
+for i in range(5):
+    piece = s[i::5]
+    im = Image.open(StringIO(piece))
+    f = open("%d.%s" %(i, im.format.lower()), "wb")
+    f.write(piece)
+    f.close()
 
-width = im.size[0]
-height = im.size[1]
-
-even = Image.new(im.mode, (width / 2, height / 2))
-odd = Image.new(im.mode, (width / 2, height / 2))
-
-for x in range(width):
-    for y in range(height):
-        pixel = im.getpixel((x, y))
-        if x % 2 ^ y % 2:
-            odd.putpixel(((x - 1) / 2, y / 2) if x % 2 else (x / 2, (y - 1) / 2), pixel)
-        else:
-            even.putpixel((x / 2, y / 2), pixel)
-even.save('cave_even.jpg')
-odd.save('cave_odd.jpg')
-start_url = 'http://www.pythonchallenge.com/pc/return/5808.html'
+start_url = 'http://www.pythonchallenge.com/pc/return/evil.html'
 # end_url = start_url.replace('bull', passcodes)
 # print end_url
