4c077824f1b97f5b5b07b186aa91765c43461036
557703988@139.com <557703988@139.com>
2017-08-10 19:26:51 -0700

    python challenge 13

--- challenge.py
+++ challenge.py
@@ -2,17 +2,10 @@
 #coding=utf-8
 #author=yexiaozhu
 
-import Image
-from cStringIO import StringIO
+import xmlrpclib
+server = xmlrpclib.Server('http://www.pythonchallenge.com/pc/phonebook.php')
+print server.phone('Bert')
 
-s= open("evil2.gfx", "rb").read()
-for i in range(5):
-    piece = s[i::5]
-    im = Image.open(StringIO(piece))
-    f = open("%d.%s" %(i, im.format.lower()), "wb")
-    f.write(piece)
-    f.close()
-
-start_url = 'http://www.pythonchallenge.com/pc/return/evil.html'
+start_url = 'http://www.pythonchallenge.com/pc/return/disproportional.html'
 # end_url = start_url.replace('bull', passcodes)
 # print end_url
