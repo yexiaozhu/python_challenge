23f464de307f9ab0b79728d6b1da6a26dc416e48
557703988@139.com <557703988@139.com>
2017-08-10 23:10:35 -0700

    python challenge 16

--- challenge.py
+++ challenge.py
@@ -2,16 +2,19 @@
 #coding=utf-8
 #author=yexiaozhu
 
-# second youngest
-# 1**6.1.27. it might be a birthday, beacuse we should buy flower for that day.
-from calendar import isleap
-from datetime import date
-TUESDAY = 1
-for year in range(1006, 2000, 10):
-    t = date(year, 1, 27)
-    if isleap(year) and t.weekday() == TUESDAY:
-        print t.isoformat()
+import Image
 
-start_url = 'http://www.pythonchallenge.com/pc/return/cat.html'
+im = Image.open("mozart.gif")
+for y in range(im.size[1]):
+    #获得像素点
+    line=[im.getpixel((x, y)) for x in range(im.size[0])]
+    idx=line.index(195)
+    line=line[idx:]+line[:idx]
+    #重新排列像素点
+    [im.putpixel((x, y),line[x]) for x in range(len(line))]
+
+im.show()
+
+start_url = 'http://www.pythonchallenge.com/pc/return/mozart.html'
 # end_url = start_url.replace('bull', passcodes)
 # print end_url
