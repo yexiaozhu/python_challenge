414de50529ee1943f330a8c7fe4785ebc6ac0320
557703988@139.com <557703988@139.com>
2017-08-22 01:54:13 -0700

    python challenge 19

--- challenge.py
+++ challenge.py
@@ -1,32 +1,25 @@
 #!/usr/bin/env python 2.7.12
 #coding=utf-8
 #author=yexiaozhu
+import email
+import re
+import urllib
+import wave
+import StringIO
+import os
+import ossaudiodev
 
-# <!-- it is more obvious that what you might think -->
-# <!-- maybe consider deltas.gz -->
-import gzip, difflib
-ff = gzip.open('deltas.gz', 'r')
-deltas = ff.read()
-ff.close()
-deltas = deltas.splitlines()
-left, right = [], []
-for row in deltas:
-    left.append(row[:53])
-    right.append(row[56:])
-diff = list(difflib.ndiff(left, right))
-
-png = ['', '', '']
-for row in diff:
-    bytes = [chr(int(byte, 16)) for byte in row[2:].split()]
-    if row[0] == '-':
-        png[0] += ''.join(bytes)
-    elif row[0] == '+':
-        png[1] += ''.join(bytes)
-    elif row[0] == ' ':
-        png[2] += ''.join(bytes)
-
-for i in range(3):
-    open('out18_%d.png' % i, 'wb').write(png[i])
-start_url = 'http://www.pythonchallenge.com/pc/return/balloons.html'
-end_url = start_url.replace('return/balloons','hex/bin' )
-print end_url
+url = 'http://butter:fly@www.pythonchallenge.com/pc/hex/bin.html'
+src = urllib.urlopen(url).read()
+c = re.compile("<!--\n(.*)\n-->", re.DOTALL)
+data = c.findall(src)[0]
+message = email.message_from_string(data)
+audio = message.get_payload(0).get_payload(decode=True)
+wav = wave.open(StringIO.StringIO(audio))
+print wav
+print wav.getsampwidth() # 2 bytes, 16bits
+format = ossaudiodev.AFMT_S16_BE # Big Endian
+channels = 2
+dsp = ossaudiodev.open('w')
+dsp.setparameters(format, channels, wav.getframerate())
+dsp.write(audio)
