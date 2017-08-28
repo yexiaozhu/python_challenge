c0855d8feb9eb28eeaa1c53e6dd800de313a85e6
557703988@139.com <557703988@139.com>
2017-08-27 19:13:25 -0700

    python challenge 20

--- challenge.py
+++ challenge.py
@@ -9,17 +9,4 @@ import StringIO
 import os
 import ossaudiodev
 
-url = 'http://butter:fly@www.pythonchallenge.com/pc/hex/bin.html'
-src = urllib.urlopen(url).read()
-c = re.compile("<!--\n(.*)\n-->", re.DOTALL)
-data = c.findall(src)[0]
-message = email.message_from_string(data)
-audio = message.get_payload(0).get_payload(decode=True)
-wav = wave.open(StringIO.StringIO(audio))
-print wav
-print wav.getsampwidth() # 2 bytes, 16bits
-format = ossaudiodev.AFMT_S16_BE # Big Endian
-channels = 2
-dsp = ossaudiodev.open('w')
-dsp.setparameters(format, channels, wav.getframerate())
-dsp.write(audio)
+url = 'http://butter:fly@www.pythonchallenge.com/pc/hex/idiots.html'
