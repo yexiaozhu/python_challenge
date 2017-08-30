5c013c71dd1263b639b7b0ea9652c2f7149ecd00
557703988@139.com <557703988@139.com>
2017-08-29 23:08:05 -0700

    python challenge 22

--- challenge.py
+++ challenge.py
@@ -2,31 +2,20 @@
 #coding=utf-8
 #author=yexiaozhu
 
-import zlib
-import binascii
-import bz2
-from zlib import decompress
-from bz2 import BZ2Decompressor
-
-zh = 'x\x9c'
-bh = 'BZ'
-
-result = []
-
-f = open('package.pack', 'rb')
-s = f.read()
-
-while True:
-        h = s[:2]
-        if h == zh:
-                s = decompress(s)
-                result.append(' ')
-        elif h == bh:
-                s = BZ2Decompressor().decompress(s)
-                result.append('#')
-        elif s[-1:-3:-1] in [zh, bh]:
-                s = s[::-1]
-                result.append('\n')
-        else:
-                break
-print ''.join(result)
+import Image, ImageDraw, ImageSequence
+img = Image.open('white.gif')
+new = Image.new('RGB', (200, 200), 'black')
+newimg = ImageDraw.Draw(new)
+x = 0
+y = 0
+for s in ImageSequence.Iterator(img):
+    l,u,r,d = img.getbbox()
+    dx = (l - 100)
+    dy = (u - 100)
+    x += dx
+    y += dy
+    if dx == dy == 0:
+        x += 30
+        y += 30
+    newimg.point((x, y))
+new.save('out22.png')
