a865a256022607ca59cf2bb4f04bf61a60e89d10
557703988@139.com <557703988@139.com>
2017-08-31 23:16:54 -0700

    python challenge 26

--- challenge.py
+++ challenge.py
@@ -2,13 +2,30 @@
 #coding=utf-8
 #author=yexiaozhu
 
-# <!-- can you see the waves? -->
-import urllib, wave, Image
-from cStringIO import StringIO
-canvas = Image.new('RGB', (300, 300))
-url = "http://butter:fly@www.pythonchallenge.com/pc/hex/lake%i.wav"
-for i in range(25):
-    wav = wave.open(StringIO(urllib.urlopen(url % (i + 1)).read()))
-    piece = Image.frombytes('RGB', (60, 60), wav.readframes(10800))
-    canvas.paste(piece, ((i % 5) * 60, (i / 5) * 60))
-canvas.show()
+# <!-- you've got his e-mail -->
+# import md5
+#
+# f = open('mybroken.zip', 'rb').read()
+# for i in range(len(f)):
+#     for j in range(256):
+#         newtext = f[:i]+chr(j)+f[i+1:]
+#         if md5.md5(newtext).hexdigest() == 'bbb8b499a0eef99b52c7f13f4e78c24b':
+#             open('mybroken_new.zip', 'wb').write(newtext)
+
+import md5, array
+
+def sub(data, good_md5):
+    allchars = map(chr, range(256))
+    for i, oldch in enumerate(data):
+        for newch in allchars:
+            data[i] = newch
+            if md5.new(data).hexdigest() == good_md5:
+                return True
+        data[i] = oldch
+    return False
+
+data = array.array("c", open("mybroken.zip", "rb").read())
+sub(data, "bbb8b499a0eef99b52c7f13f4e78c24b")
+f = open("repaired.zip", "wb")
+f.write(data)
+f.close()
