d5ab2c15f0b6173c651ea860ee58102ba025449a
557703988@139.com <557703988@139.com>
2017-08-31 23:40:04 -0700

    python challenge 27

--- challenge.py
+++ challenge.py
@@ -2,30 +2,23 @@
 #coding=utf-8
 #author=yexiaozhu
 
-# <!-- you've got his e-mail -->
-# import md5
-#
-# f = open('mybroken.zip', 'rb').read()
-# for i in range(len(f)):
-#     for j in range(256):
-#         newtext = f[:i]+chr(j)+f[i+1:]
-#         if md5.md5(newtext).hexdigest() == 'bbb8b499a0eef99b52c7f13f4e78c24b':
-#             open('mybroken_new.zip', 'wb').write(newtext)
-
-import md5, array
-
-def sub(data, good_md5):
-    allchars = map(chr, range(256))
-    for i, oldch in enumerate(data):
-        for newch in allchars:
-            data[i] = newch
-            if md5.new(data).hexdigest() == good_md5:
-                return True
-        data[i] = oldch
-    return False
-
-data = array.array("c", open("mybroken.zip", "rb").read())
-sub(data, "bbb8b499a0eef99b52c7f13f4e78c24b")
-f = open("repaired.zip", "wb")
-f.write(data)
-f.close()
+# <!-- did you say gif? -->
+# <!-- oh, and this is NOT a repeat of 14 -->
+import Image, string, bz2, keyword
+f = Image.open('zigzag.gif')
+fd = f.tobytes()
+fp = f.palette.getdata()[1][::3]
+trans = string.maketrans(''.join([chr(i) for i in range(256)]), fp)
+ftran = fd.translate(trans)
+diff = ['', '']
+img = Image.new('1', f.size, 0)
+for i in range(1, len(fd)):
+    if fd[i] != ftran[i-1]:
+        diff[0] += fd[i]
+        diff[1] += ftran[i-1]
+        img.putpixel(((i-1)%f.size[0], (i-1)/f.size[0]), 1)
+img.save('out27.png')
+text = bz2.decompress(diff[0])
+for i in text.split():
+    if not keyword.iskeyword(i):
+        print i
